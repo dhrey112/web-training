48f08bdeb255300069fb40c354fa12109eed598e
dhrey <d.adeleke@semicolon.africa>
2021-05-15 12:24:21 +0100

    function and string added

--- JAVASCRIPT/function.js
+++ JAVASCRIPT/function.js
@@ -2,6 +2,7 @@ function displayFunc(){
     console.log("Hello, I am a function")
 }
 
+// Function with parameter and argument
 displayFunc();
 
 function sumFunc(a, b){
@@ -10,3 +11,44 @@ function sumFunc(a, b){
 }
 
 sumFunc(2, 65)
+
+// Function with return statement
+function theFunc(a, b){
+    return (a+b);
+}
+
+var aVar = theFunc(3, 8)
+console.log(aVar);
+
+// Function as a value
+function circleArea(radius){
+    var result = 3.14 * (radius**2);
+    return result;
+}
+console.log("The area of the circle is: "+circleArea(10));
+
+
+// Scope
+var gVar = "Hello";
+console.log(gVar);
+
+function myFunc(){
+    gVar = "World";
+    console.log(gVar);
+}
+
+myFunc();
+console.log(gVar);
+
+
+
+function theFunc(){
+    var localVar = "I am local";
+    console.log(localVar);
+}
+
+theFunc();
+console.log(localVar);
+
+
+
---
+++ JAVASCRIPT/string.js
@@ -0,0 +1,7 @@
+var myString = `The boy is just ${20+2}`;
+
+console.log(myString)
+
+var myVal = 20;
+var myString2 = `I have ${myVal} apples`
+console.log(myString2)
